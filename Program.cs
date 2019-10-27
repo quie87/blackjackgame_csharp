d61226b33cbb8ab08078ac3601c0a2e7cf6a54d6
Kristoffer Åberg <kristoffer.aberg12@gmail.com>
2019-10-27 20:14:06 +0100

    adding assignment start files

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BlackJack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            model.Game g = new model.Game();
+            view.IView v = new view.SimpleView(); // new view.SwedishView();
+            controller.PlayGame ctrl = new controller.PlayGame();
+
+            while (ctrl.Play(g, v));
+        }
+    }
+}
